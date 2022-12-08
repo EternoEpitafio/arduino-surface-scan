7501ed3bb0a3990e7bf7749f1c99b4aab834fcd8
EternoEpitafio <leonardogabriel64@gmail.com>
2022-12-07 22:51:39 -0300

    Create ArduinoSurfaceScan.ino

---
+++ ArduinoSurfaceScan/ArduinoSurfaceScan.ino
@@ -0,0 +1,158 @@
+//Programa: Varredura de Superfície
+//Autor: Leonardo Gabriel Cardoso de Moraes
+//Curso: Engenharia da Computação
+//Intituição: UFPA
+
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+
+//Define os pinos para o trigger e echo
+#define pino_trigger 2
+#define pino_echo 3
+
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+//Define o pino para o botão
+#define botao 13
+
+//int distancia = 0;
+//int tempo = 0;
+
+//Variáveis de controle
+String x = "x";
+String y = "y";
+int xi = 0;
+int yi = 0;
+int continua = 0;
+
+//Marcador de início de mensagem
+String valString = "<";
+
+//Define os pinos dos motores
+#define motorXf 5
+#define motorXt 4
+#define motorXa 6
+
+#define motorYf 10
+#define motorYt 8
+#define MotorYa 9
+
+void giroHorario(String motor) {
+  if (motor == "x" ) {
+    digitalWrite(motorXa, HIGH);
+    digitalWrite(motorXf, HIGH);
+    digitalWrite(motorXt, LOW);
+  } else if (motor == "y") {
+    digitalWrite(MotorYa, HIGH);
+    digitalWrite(motorYf, HIGH);
+    digitalWrite(motorYt, LOW);
+  }
+}
+
+void giroAntiHorario(String motor) {
+  if (motor == "x" ) {
+    digitalWrite(motorXa, HIGH);
+    digitalWrite(motorXf, LOW);
+    digitalWrite(motorXt, HIGH);
+  } else if (motor == "y") {
+    digitalWrite(MotorYa, HIGH);
+    digitalWrite(motorYf, LOW);
+    digitalWrite(motorYt, HIGH);
+  }
+}
+
+void desligaMotor(String motor) {
+  if (motor == "x" ) {
+    digitalWrite(motorXa, LOW);
+    digitalWrite(motorXf, LOW);
+    digitalWrite(motorXt, LOW);
+  } else if (motor == "y") {
+    digitalWrite(MotorYa, LOW);
+    digitalWrite(motorYf, LOW);
+    digitalWrite(motorYt, LOW);
+  }
+}
+
+float cm() {
+  //Le as informacoes do sensor, em cm
+  float cmMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  return cmMsec;
+}
+
+void setup()
+{
+  pinMode(botao, INPUT_PULLUP);
+  Serial.begin(9600);
+
+  pinMode(motorXt, OUTPUT);  // para trás
+  pinMode(motorXf, OUTPUT);  // para frente
+  pinMode(motorXa, OUTPUT);  // aciona o motor A
+
+  pinMode(motorYt, OUTPUT);  // para trás
+  pinMode(motorYf, OUTPUT);  // para frente
+  pinMode(MotorYa, OUTPUT);  // aciona o motor B
+}
+
+void loop()
+{
+  //Inicia a varredura ao pressionar o botão
+  if (!digitalRead(botao)) {
+    continua = 0;
+    delay(2000);
+    while (continua == 0) {
+      for (int j = 1; j <= 6; j++) {
+        for (int i = 1; i <= 4; i++) {
+          xi = i;
+          yi = j;
+          giroHorario(x);
+          delay(1000);
+          desligaMotor(x);
+          delay(100);
+          //Concatena as informações e exibe no serial monitor
+          valString.concat(xi);
+          valString.concat(",");
+          valString.concat(yi);
+          valString.concat(",");
+          valString.concat(cm());
+          valString.concat(">"); //Marcador de fim de mensagem
+          Serial.print(valString); //Imprime as coordenadas
+          valString = "<";
+          if (i == 4) {
+            giroHorario(y);
+            delay(1000);
+            desligaMotor(y);
+            delay(100);
+            j = j+1;
+            yi = j;
+          }
+        }
+        for (int i = 4; i >= 1; i--) {
+          xi = i;
+          yi = j;
+          giroAntiHorario(x);
+          delay(1000);
+          desligaMotor(x);
+          delay(100);
+          //Concatena as informações e exibe no serial monitor
+          valString.concat(xi);
+          valString.concat(",");
+          valString.concat(yi);
+          valString.concat(",");
+          valString.concat(cm());
+          valString.concat(">"); //Marcador de fim de mensagem
+          Serial.print(valString); //Imprime as coordenadas
+          if (i == 1) {
+            giroHorario(y);
+            delay(1000);
+            desligaMotor(y);
+            delay(100);
+          }
+        }
+      }
+      continua = 1;
+    }
+  }
+}
